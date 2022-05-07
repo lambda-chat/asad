b43f3faf00c6c7b930087d5d19b31d8c2a3550b8
Λ__Λ <chat.algebrique@gmail.com>
2022-05-08 01:48:58 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const App: FC = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+    </header>
+  </div>
+);
+
+export default App;
